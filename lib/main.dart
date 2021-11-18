841a68168a3faf91161916508c8ad1c0689cafc1
abiFarhan <abifarhan04@gmail.com>
2021-11-18 11:43:46 +0700

    Adding Button to implement onClick

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,17 @@ class DicePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Row(
-          children: <Widget>[
-            Expanded(child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/dice1.png'),
-            )),
-            Expanded(child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('images/dice1.png'),
-            ))
-          ],
-        )
-    );
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {},
+            child: Image.asset('images/dice1.png'),
+          ),
+        ),
+        Expanded(
+            child: FlatButton(onPressed: () {  },
+            child: Image.asset('images/dice1.png')))
+      ],
+    ));
   }
 }
